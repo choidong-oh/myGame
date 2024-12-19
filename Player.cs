2eeb66839b7401812d9fc3758479e4ecc0024009
choidong-oh <qltjfeo@naver.com>
2024-12-19 17:57:30 +0900

    Update Player.cs

--- Player.cs
+++ Player.cs
@@ -10,14 +10,19 @@ namespace MyGame
     {
         //PLAYER의 카드가 존재해야함
         //리스트를 하고 그 중 맨윗값 추가함
-        public Deck deck;
-        List<Deck> decks = new List<Deck>();
+        public Deck deck= new Deck();
+        List<Card> decks = new List<Card>();
         
         
         public Player()
         {
-            deck = new Deck();
-            decks.Add(deck.usedeck[0]);
+            if (deck.usedeck[0] == null)
+            {
+                //첫장 둘째장 player리스트에 추가
+                decks.Add(deck.usedeck[0]);
+                decks.Add(deck.usedeck[1]);
+
+            }
 
 
         }
