febf064b1700126cabe680bfee3a9216af9087fd
choidong-oh <qltjfeo@naver.com>
2024-12-27 15:22:57 +0900

    마무리 메모

--- Program.cs
+++ Program.cs
@@ -10,8 +10,24 @@ namespace MyGame
 {
     internal class Program
     {
+
         static void Main(string[] args)
         {
+
+            //            메인화면
+            //기본배팅
+            //카드분배돼면
+            //폴드로지는거보여주고
+            //콜로 이기는거 보여주고
+            //올인을박고
+            //다음배팅때 하프를 눌르면안되는거 보여주고
+            //콜박고
+            //38광떙으로 이기고 마무리
+
+            //추가로멍터구리구사
+            //예외처리 땡잡이 보야주면댐
+
+
             //사이드팟, ai배팅
             GameMaster gameMaster = new GameMaster();
             Player myplayer = new Player(100000);
@@ -32,7 +48,6 @@ namespace MyGame
                     }
                 }
             }
-
 
 
 
